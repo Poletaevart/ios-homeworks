403600ce03c7d8404893c460904a12697811da68
Poletaevart <artem72.com@gmail.com>
2022-07-16 23:23:32 +0500

    Настройка tab bar

---
+++ Navigation/FeedViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FeedViewController.swift
+//  Navigation
+//
+//  Created by Artem Poletaev on 16.07.2022.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController{
+    
+    override func viewDidLoad() {
+
+            super.viewDidLoad()
+
+            // Задаем фоновый цвет
+
+            view.backgroundColor = .white
+
+    }
+}
---
+++ Navigation/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Artem Poletaev on 16.07.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController{
+    override func viewDidLoad() {
+
+            super.viewDidLoad()
+
+            // Задаем фоновый цвет
+
+            view.backgroundColor = .yellow
+
+    }
+}
