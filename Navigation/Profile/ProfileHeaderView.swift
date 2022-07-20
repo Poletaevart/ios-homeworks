70dd5452109d0676da908dbeeef131e25ee2c967
Poletaevart <artem72.com@gmail.com>
2022-07-20 22:32:47 +0500

    Создание нового Profile View Controller

---
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by Artem Poletaev on 20.07.2022.
+//
+
+import UIKit
+
+class ProfileHeaderView:UIView{
+    
+}
---
+++ Navigation/Profile/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Artem Poletaev on 20.07.2022.
+//
+
+import UIKit
+
+class ProfileViewController:UIViewController{
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .lightGray
+    }
+}
