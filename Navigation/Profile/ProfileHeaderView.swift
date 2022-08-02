130488d96f20d36f55ed4bb0712b1acffa5ff91b
Poletaevart <artem72.com@gmail.com>
2022-08-02 17:55:24 +0500

    Смена статуса по кнопке

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -27,13 +27,25 @@ class ProfileHeaderView:UIView{
         return textLabel
     }()
     
+    private lazy var textLabel2: UILabel = {
+        let textLabel2 = UILabel(frame: CGRect(x: 218, y: 100, width: 137, height: 60))
+        textLabel2.text = "Waiting for something"
+        textLabel2.textColor = .gray
+        textLabel2.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        return textLabel2
+    }()
+    
     private lazy var textFild: UITextField = {
-        let textFild = UITextField(frame: CGRect(x: 218, y: 112, width: 137, height: 60))
-        textFild.text = "Waiting for something"
-        textFild.textColor = .gray
-        textFild.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        let textFild = UITextField(frame: CGRect(x: 218, y: 140, width: 160, height: 40))
+        textFild.textColor = .black
+        textFild.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        textFild.layer.borderWidth = 1
+        textFild.layer.borderColor = UIColor(ciColor: .black).cgColor
+        textFild.layer.cornerRadius = 12
+        textFild.backgroundColor = .white
+        textFild.addTarget(self, action: #selector(statusTextChanged), for: .editingChanged)
         return textFild
-}()
+        }()
     private lazy var button: UIButton = {
         let button = UIButton(frame: CGRect(x: 20, y: 206, width: 380, height: 50))
         button.backgroundColor = .systemBlue
@@ -59,15 +71,23 @@ class ProfileHeaderView:UIView{
     private func setupView() {
         self.addSubview(self.avatarImageView)
         self.addSubview(self.textLabel)
+        self.addSubview(self.textLabel2)
         self.addSubview(self.textFild)
         self.addSubview(self.button)
     
     }
     
     @objc func buttonPressed(){
-        if let text = textFild.text {
+        if let text = textLabel2.text {
         print(text)}
         else {
         print("текущее поле пустое")}
+        textLabel2.text = statusText
 }
+   
+    @objc func statusTextChanged(_ textField: UITextField){
+        statusText = textField.text!
+    }
+    private var statusText:String = ""
+
 }
