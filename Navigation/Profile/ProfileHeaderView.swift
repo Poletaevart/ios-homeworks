2e6986e659c920397dad912b078c2d7351eb890c
Poletaevart <artem72.com@gmail.com>
2022-08-18 22:07:16 +0500

    тест

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -116,7 +116,7 @@ class ProfileHeaderView:UIView{
         if let text = statusTextLabel.text {
             print(text)}
         else {
-            print("текущее поле пустое")}
+            print("текущее поле пустое 0")}
         statusTextLabel.text = statusText
     }
     
