f607cb14f0e6b19801df06705beefbfbcd9252ab
Poletaevart <artem72.com@gmail.com>
2022-09-05 20:09:28 +0500

    Добавление таблици

---
+++ Navigation/Profile/Post.swift
@@ -0,0 +1,17 @@
+//
+//  Post.swift
+//  Navigation
+//
+//  Created by Artem Poletaev on 17.07.2022.
+//
+
+import UIKit
+
+struct Post {
+    
+    var author: String
+    var description: String
+    var image: String
+    var views: Int
+    var likes: Int
+}
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -7,47 +7,76 @@
 
 import UIKit
 
-class ProfileViewController:UIViewController{
+class ProfileViewController: UIViewController{
     
-    private lazy var profileHeaderView: ProfileHeaderView = {
-        let profileHeaderView = ProfileHeaderView()
-        self.view.backgroundColor = .lightGray
-        profileHeaderView.translatesAutoresizingMaskIntoConstraints = false
-        return profileHeaderView
+    private var viewModel: [Post] = [
+        Post(author: "Очень странные дела", description: "Сериал о двух мирах", image: "Post1", views: 1000, likes: 900),
+        Post(author: "Удача", description: "Мультик про кота и девочку без удачи", image: "Post2", views: 500, likes: 450),
+        Post(author: "Медленные лошади", description: "Сериал про неудачников из m5", image: "Post3", views: 700, likes: 600),
+        Post(author: "Не сработало", description: "Сериал про стартап", image: "Post4", views: 455, likes: 355)]
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(ProfileTableHeaderView.self, forHeaderFooterViewReuseIdentifier: "HeaderView")
+        tableView.register(PostTableViewCell.self, forCellReuseIdentifier: "PostCell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
     }()
     
-    private lazy var newTitleButton: UIButton = {
-            let button = UIButton()
-            button.setTitle("New title", for: .normal)
-            button.setTitleColor(.white , for: .normal)
-            button.backgroundColor = .systemBlue
-            button.layer.shadowOffset = CGSize(width: 4, height: 4)
-            button.layer.shadowColor = CGColor(red: 0, green: 0, blue: 0, alpha: 1)
-            button.layer.shadowOpacity = 0.7
-            button.layer.shadowRadius = 4
-            button.layer.cornerRadius = 4
-            button.translatesAutoresizingMaskIntoConstraints = false
-            return button
-        }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavigationBar()
+        setupView()
+    }
+
+    private func setupNavigationBar() {
+       navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Profile"
+    }
+
+    private func setupView() {
+        view.backgroundColor = .systemBackground
         
-        self.navigationItem.title = "Profile"
-        self.view.addSubview(self.profileHeaderView)
-        self.view.addSubview(self.newTitleButton)
+        view.addSubview(tableView)
         
         NSLayoutConstraint.activate([
-            profileHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            profileHeaderView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            profileHeaderView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            profileHeaderView.heightAnchor.constraint(equalToConstant: 220),
-            
-            newTitleButton.bottomAnchor.constraint(equalTo:self.view.safeAreaLayoutGuide.bottomAnchor),
-            newTitleButton.leftAnchor.constraint(equalTo: self.view.leftAnchor),
-            newTitleButton.rightAnchor.constraint(equalTo: self.view.rightAnchor),
-            newTitleButton.heightAnchor.constraint(equalToConstant: 50)
-        
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
+    }
+}
+
+extension ProfileViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        if let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: "HeaderView") as? ProfileTableHeaderView {
+            return header
+        }
+        return nil
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        CGFloat(200.0)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as? PostTableViewCell {
+            cell.authorLabel.text = viewModel[indexPath.row].author
+            cell.postImageView.image = UIImage(named: viewModel[indexPath.row].image)
+            cell.descriptionLabel.text = viewModel[indexPath.row].description
+            cell.likesViewsLabel.text = "views: \(viewModel[indexPath.row].views) likes: \(viewModel[indexPath.row].likes)"
+            return cell
+        }
+            return UITableViewCell()
     }
 }
